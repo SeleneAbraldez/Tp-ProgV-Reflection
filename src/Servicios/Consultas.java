9e17cc99857e4230dc8696e34fcb52e56dcf1f0b
Selene <seleabraldez@gmail.com>
2020-11-10 16:46:33 -0300

    P1 compl, P3 s/doc

---
+++ src/Servicios/Consultas.java
@@ -0,0 +1,50 @@
+package Servicios;
+
+import Utilidades.UBean;
+
+import java.util.ArrayList;
+import java.lang.reflect.Field;
+
+public class Consultas {
+
+	/**
+	 * Guardar en la base de datos del objeto armando query por medio de reflexion
+	 * 
+	 * @param obj
+	 */
+	public static void guardar(Object obj) {
+
+	}
+
+	/**
+	 * modificar todas las columnas menos id ararmar query medio de reflrexion
+	 * 
+	 * @param obj
+	 */
+	public static void modificar(Object obj) {
+
+	}
+
+	/**
+	 * eliinar regstro de la base de datos ararmar query medio de reflrexion
+	 * 
+	 * @param obj
+	 */
+	public static void eliminar(Object obj) {
+
+	}
+
+	/**
+	 * Devuelve objeto de la clasee peduda con datos cargados armarse la query por
+	 * medio de reflexión
+	 * 
+	 * @param c:  clase del objeto
+	 * @param id: id del objeto pedido
+	 * @return Object: objeto con datos
+	 */
+	public static Object obtenerPorId(Class c, Object id) {
+		return null;
+	}
+	
+	
+}
---
+++ src/Utilidades/UBean.java
@@ -0,0 +1,89 @@
+package Utilidades;
+
+import java.util.ArrayList;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class UBean {
+
+	/**
+	 * Devuelve los atributos del objeto pasado
+	 * 
+	 * @param obj:			 		objeto pasado para averiguar atributos
+	 * @return ArrayList<Field>: 	arraylist con todos los atributos
+	 */
+	public static ArrayList<Field> obtenerAtributos(Object o) {
+		ArrayList<Field> listAtts = new ArrayList<Field>();
+		Class c = o.getClass();
+
+		Field[] atts = c.getDeclaredFields();
+		for (Field f : atts) {
+			listAtts.add(f);
+		}
+
+		return listAtts;
+	}
+
+	/**
+	 * Ejecuta set del objeto pasado del atributo pasado con valor especifico
+	 * 
+	 * @param o:     objeto a ejecutar
+	 * @param att:   atributo que se quiere settaer
+	 * @param valor: valor a settear
+	 */
+	public static void ejecutarSet(Object o, String att, Object valor) {
+		Class c = o.getClass();
+		String nombreAtt = att.substring(0, 1).toUpperCase() + att.substring(1);
+		Method[] metodos = c.getDeclaredMethods();
+
+		for (Method m : metodos) {
+			if (m.getName().equals("set" + nombreAtt)) {
+				Object[] params = new Object[1];
+				
+				// TODO agregaria un if de que si el tipo de atributo es int confirme que el valor apsdo sea efectivamentr un int
+				// if(m.equals(valor.getClass())) {
+				params[0] = valor;
+				// }
+				try {
+					m.invoke(o, params);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * Ejecuta get del objeto pasado del atributo pasado
+	 * 
+	 * @param o:     objeto a ejecutar
+	 * @param att:   atributo que se quiere traer
+	 * @return Object: objeto con valor del att
+	 */
+	public static Object ejecutarGet(Object o, String att) {
+		Object valorAtt = null;
+		Class c = o.getClass();
+		String nombreAtt = att.substring(0, 1).toUpperCase() + att.substring(1);
+		Method[] metodos = c.getDeclaredMethods();
+
+		for (Method m : metodos) {
+			if (m.getName().equals("get" + nombreAtt)) {
+				try {
+					valorAtt = m.invoke(o, null);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+		
+		return valorAtt;
+	}
+
+
+
+}
